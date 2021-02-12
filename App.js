8717b0bcdf857a292212bf6b0fcab62a7ff98577
ROMAN KOSOLAPOV <roman.kosolapov@cashelec.ru>
2021-02-12 17:42:51 +0300

    Update links

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import shared from "shared";
-
+import {round, swapCourse} from '@helpers'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>!{shared}!</Text>
+      <Text>Rounded 1.123123: {round(99.99999, 3)}!</Text>
+      <Text>Swapp course 0,002: {swapCourse(0.1)}!</Text>
       <StatusBar style="auto" />
     </View>
   );
--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,15 @@
-const { createMetroConfiguration } = require('expo-yarn-workspaces');
+const path = require('path');
+module.exports = {
 
-module.exports = createMetroConfiguration(__dirname);
+	resolver: {
+		extraNodeModules: new Proxy({}, {
+			get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
+		}),
+	},
+
+	projectRoot: path.resolve(__dirname),
+	watchFolders: [
+	// here you can add your shared module to make the watcher to be able to see it 
+		path.resolve(__dirname, "../shared") 
+	]
+}
